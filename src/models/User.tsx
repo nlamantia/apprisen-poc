68d00c80916b0fb1aa1be215bfc44dd77e61ef2d
nlamantia <lamantia.27@osu.edu>
2019-12-06 13:20:55 -0500

    Initial commit.

---
+++ src/models/User.tsx
@@ -0,0 +1,10 @@
+export class User {
+    userId: number = 0;
+    firstName: string = "";
+    lastName: string = "";
+    address: string = "";
+    city: string = "";
+    state: string = "";
+    zip: string = "";
+    email: string = "";
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,55 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonItem } from '@ionic/react';
+import React, { Component } from 'react';
+import { tsConstructorType } from '@babel/types';
+import { User } from '../models/User';
 
-const Home: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Ionic Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="ion-padding">
-        The world is your oyster.
-        <p>
-          If you get lost, the{' '}
-          <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/">
-            docs
-          </a>{' '}
-          will be your guide.
-        </p>
-      </IonContent>
-    </IonPage>
-  );
+class Home extends Component {
+  state = {
+    users: [] as User[]
+  };
+
+  componentDidMount() {
+    fetch("http://localhost:8080/api/users")
+    .then(response => response.json())
+    .then(responseJson => {
+      console.log(responseJson)
+      this.setState({
+        users: responseJson as User[]
+      });
+    })
+    .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Ionic Blank</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className="ion-padding">
+          <IonList>
+            {
+              this.state.users.map(user => {
+                return (
+                  <IonItem>{user.firstName + ' ' + user.lastName}</IonItem>
+                )
+              })
+            }
+          </IonList>
+          {/* The world is your oyster.
+          <p>
+            If you get lost, the{' '}
+            <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/">
+              docs
+            </a>{' '}
+            will be your guide.
+          </p> */}
+        </IonContent>
+      </IonPage>
+    )
+  };
 };
 
 export default Home;
