17a82dc56986cc9c8ce7de5f646c1e4153b7a26c
nlamantia <nicholas.f.lamantia@tsgforce.com>
2020-04-17 08:45:40 -0400

    Generic layout for the make payment page

--- src/pages/payment/make-payment.tsx
+++ src/pages/payment/make-payment.tsx
@@ -1,5 +1,5 @@
 import {
-    IonBackButton,
+    IonBackButton, IonButton,
     IonButtons,
     IonCard,
     IonCol,
@@ -16,19 +16,51 @@ import {
     IonTitle,
     IonToolbar
 } from "@ionic/react";
-import React from "react";
+import {connect} from 'react-redux'
+import React, {useEffect} from "react";
+import {bindActionCreators} from "redux";
+import {getClientAccountData} from "../../feature/payment/action";
 // eslint-disable-next-line
-import Menu from "../menu/menu";
 
 
-const MakePayment = ( props ) => {
-    // const { caseSummary, debts } = props
-    // const { estimatedBalance, currentMonthlyPayment, totalMonthlyDeposit } = caseSummary
+const _MakePayment = ( props ) => {
+    const { clientAccountData, getClientAccountData } = props;
+    const { credentials } = props;
+
+    const { bankAccountTypes } = clientAccountData;
+    const { linkedApplication: [{}, { externalId }] } = credentials;
+    console.log("External ID: " + externalId);
+
+    let date = new Date();
+    date.setMonth(3);
+    date.setFullYear(2029);
+    date.setDate(27);
+
+    const printDate = (date) => {
+        if (date && date.getMonth() && date.getDate() && date.getFullYear()) {
+            const month = date.getMonth() + 1;
+            const day = date.getDate();
+            const year = date.getFullYear();
+            return year + "-" + printTwoDigitNumber(month) + "-" + printTwoDigitNumber(day);
+        } else {
+            return "";
+        }
+    };
+
+    const printTwoDigitNumber = (num: number) => {
+        return num < 10 ? "0" + num : num;
+    }
+
+    useEffect(() => {
+        if (!clientAccountData || !bankAccountTypes) {
+            console.log("No client account data. Fetching...");
+            getClientAccountData();
+        }
+    });
 
     return (
         <>
             {/*<Menu pageName={'accountOverview'} /> todo fix this*/}
-            <Menu />
             <IonPage>
                 <IonHeader>
                     <IonToolbar>
@@ -54,37 +86,103 @@ const MakePayment = ( props ) => {
                                         </IonListHeader>
                                         <IonItem>
                                             <IonLabel>
-                                                <h3>Current Balance</h3>
+                                                <h3>Case Number</h3>
                                             </IonLabel>
                                             <IonLabel>
                                                 <h3 className={"ion-text-right"}>
-                                                    $15000
+                                                    {externalId}
                                                 </h3>
                                             </IonLabel>
                                         </IonItem>
                                         <IonItem>
                                             <IonLabel>
                                                 <h3>
-                                                    Monthly Payment
+                                                    Payment Amount
                                                 </h3>
                                             </IonLabel>
                                             <IonLabel>
                                                 <h3 className={"ion-text-right"}>
-                                                    $400
+                                                    {'<'}Input{'>'}
                                                 </h3>
                                             </IonLabel>
                                         </IonItem>
                                         <IonItem>
                                             <IonLabel>
                                                 <h3>
-                                                    Total Monthly Deposit
+                                                    Payment Date
                                                 </h3>
                                             </IonLabel>
                                             <IonLabel>
                                                 <h3 className={"ion-text-right"}>
-                                                    $400
+                                                    {printDate(date)}
                                                 </h3>
                                             </IonLabel>
+                                        </IonItem>
+                                        <IonItem>
+                                            <IonLabel>
+                                                <h3>
+                                                    Payment Comments
+                                                </h3>
+                                            </IonLabel>
+                                            <IonLabel>
+                                                <h3 className={"ion-text-right"}>
+                                                    {'<'}Input{'>'}
+                                                </h3>
+                                            </IonLabel>
+                                        </IonItem>
+                                    </IonList>
+                                </IonCard>
+                            </IonCol>
+                        </IonRow>
+                        <IonRow>
+                            <IonCol size={"12"} sizeMd={"6"} offsetMd={"3"}>
+                                <IonCard>
+                                    <IonList class="ion-no-padding">
+                                        <IonListHeader class={"white ion-text-center ion-padding-end"}>
+                                            <IonLabel>
+                                                <h2>Banking Information</h2>
+                                            </IonLabel>
+                                        </IonListHeader>
+                                        <IonItem>
+                                            <IonLabel>
+                                                <h3>Account Type</h3>
+                                            </IonLabel>
+                                            <h3 className={"ion-text-right"}>
+                                                {'<'}Drop-Down{'>'}
+                                            </h3>
+                                        </IonItem>
+                                        <IonItem>
+                                            <IonLabel>
+                                                <h3>Bank Routing #</h3>
+                                            </IonLabel>
+                                            <h3 className={"ion-text-right"}>
+                                                {'<'}Input{'>'}
+                                            </h3>
+                                        </IonItem>
+                                        <IonItem>
+                                            <IonLabel>
+                                                <h3>Bank Account #</h3>
+                                            </IonLabel>
+                                            <h3 className={"ion-text-right"}>
+                                                {'<'}Input{'>'}
+                                            </h3>
+                                        </IonItem>
+                                        <IonItem>
+                                            <IonLabel>
+                                                <h3>Primary Name on Account</h3>
+                                            </IonLabel>
+                                            <h3 className={"ion-text-right"}>
+                                                {'<'}Drop-Down{'>'}
+                                                {'<'}Input{'>'}
+                                            </h3>
+                                        </IonItem>
+                                        <IonItem>
+                                            <IonButton slot={'start'}>
+                                                <b>Cancel</b>
+                                            </IonButton>
+                                            <IonButton slot={'end'}>
+                                                <b>Submit</b>
+                                            </IonButton>
                                         </IonItem>
                                     </IonList>
                                 </IonCard>
@@ -97,17 +195,16 @@ const MakePayment = ( props ) => {
     )
 };
 
-// const AccountOverview = connect(
-//     state => ({
-//         caseSummary: state.case.caseSummary,
-//         debts: state.debt.debts
-//     }),
-//     dispatch => bindActionCreators({
-//         getCaseSummary,
-//         getDebts
-//     }, dispatch)
-// )(
-//     _AccountOverview
-// );
+const MakePayment = connect(
+    state => ({
+        credentials: state.auth.credentials,
+        clientAccountData: state.payment.clientAccountData
+    }),
+    dispatch => bindActionCreators({
+        getClientAccountData
+    }, dispatch)
+)(
+    _MakePayment
+);
 
 export default MakePayment
