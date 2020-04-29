5cbcabc3f8e34e9b5784ae26cb9449e767f82000
nlamantia <nicholas.f.lamantia@tsgforce.com>
2020-04-29 08:58:39 -0400

    Added support for when no debts are found in lender list on overview page

--- src/pages/overview/lender-list.tsx
+++ src/pages/overview/lender-list.tsx
@@ -7,29 +7,30 @@ import {Link} from "react-router-dom";
 import {connect} from 'react-redux'
 import {bindActionCreators} from "redux";
 import {getDebts, selectDebt} from "../../feature/debt/action";
-import { InAppBrowser } from "@ionic-native/in-app-browser";
-import { getClientInformation } from "feature/client/action";
+import {InAppBrowser} from "@ionic-native/in-app-browser";
+import {getClientInformation} from "feature/client/action";
 
-const _LenderList = (props : any) => {
+const _LenderList = (props: any) => {
 
 
-    const { debts, selectDebt } = props;
+    const {debts, selectDebt} = props;
 
     const handleUploadStatementClick = () => {
-        InAppBrowser.create("https://clientportal.apprisen.com",'_system', 'location=yes');
+        InAppBrowser.create("https://clientportal.apprisen.com", '_system', 'location=yes');
     }
 
     console.log(debts)
-        return (
-            <IonCard class="color">
-                <IonList class="ion-no-padding">
-                    <IonListHeader class={"white"}>
-                        <IonLabel>
-                            <h2>Lenders</h2>
-                        </IonLabel>
-                    </IonListHeader>
+    return (
+        <IonCard class="color">
+            <IonList class="ion-no-padding">
+                <IonListHeader class={"white"}>
+                    <IonLabel>
+                        <h2>Lenders</h2>
+                    </IonLabel>
+                </IonListHeader>
 
-                    {debts && debts.map((caseDebt: CaseDebt, i: any) => {
+                {debts && debts.length > 0
+                    ? debts.map((caseDebt: CaseDebt, i: any) => {
                         return (
                             <IonItem key={i}>
                                 <IonLabel>
@@ -39,21 +40,28 @@ const _LenderList = (props : any) => {
                                 <Link
                                     to={{
                                         pathname: `/lender-overview`,
-                                        state: { lender: caseDebt }
+                                        state: {lender: caseDebt}
                                     }}
                                     onClick={() => selectDebt(caseDebt.$id)}
                                 >
-                                    <IonButton onClick={() => selectDebt(caseDebt.$id)} fill={'clear'} className={'lender-button'}>Info</IonButton>
+                                    <IonButton onClick={() => selectDebt(caseDebt.$id)} fill={'clear'}
+                                               className={'lender-button'}>Info</IonButton>
                                 </Link>
                             </IonItem>
                         )
-                    })}
-                </IonList>
-                <IonItem className={'full-button'}>
-                        <IonButton className={'full-button'} expand="full" onClick={handleUploadStatementClick}>Upload Statement</IonButton>
-                </IonItem>  
-            </IonCard>
-        )
+                    })
+                    : <IonItem>
+                        <IonLabel>
+                            <h3 className={'full-center'}>No debts found</h3>
+                        </IonLabel>
+                    </IonItem>}
+            </IonList>
+            <IonItem className={'full-button'}>
+                <IonButton className={'full-button'} expand="full" onClick={handleUploadStatementClick}>Upload
+                    Statement</IonButton>
+            </IonItem>
+        </IonCard>
+    )
 }
 
 const LenderList = connect(
